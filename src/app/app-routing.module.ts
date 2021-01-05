3596a045005a9a8f134ef490746b4c97c16279e8
Zdravko Kolev <ZKolev@infragistics.com>
2021-01-05 16:23:58 +0200

    ng update @angular/core @angular/cli

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), ErrorRoutingModule],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }), ErrorRoutingModule],
   exports: [RouterModule, ErrorRoutingModule]
 })
 export class AppRoutingModule { }
